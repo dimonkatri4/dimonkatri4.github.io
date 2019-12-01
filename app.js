1393974177a403dd00983cbbd786fd7ffb0c31bd
Dima <katri4dimon@i.ua>
2019-12-01 12:08:18 +0200

    added likeButtons in js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 let productsCountEl = document.getElementById("products-count");
 
 let addToCartButtons = document.querySelectorAll(".add-to-cart");
-console.log(addToCartButtons);
 
 for (let i=0; i < addToCartButtons.length; i++) {
 	addToCartButtons[i].addEventListener ('click', function (){
 		productsCountEl.textContent = +productsCountEl.textContent + 1;
 		// let prevProductsCount = +productsCountEl.textContent;
 		// productsCountEl.textContent = prevProductsCount + 1;
-	})
+	}
+	)
+}
+
+let likeButtons = document.querySelectorAll(".like_btn");
+console.log(likeButtons);
+for (let i=0; i < likeButtons.length; i++) {
+	likeButtons[i].addEventListener('click', function() {
+		likeButtons.style.backgroundColor = '#2c71b8';
+
+	} )
 }
